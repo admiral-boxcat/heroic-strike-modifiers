eed02bf24b9ae0943755d127c9a8b0a9d7937ab8
admiral-boxcat <username@imac.lan>
2018-07-14 13:18:49 -0400

    reordering text

--- src/App.js
+++ src/App.js
@@ -28,11 +28,12 @@ class App extends Component {
         <h2>{mod1[weeksSinceLaunch%mod1.length]}</h2>
         <h2>{mod2[daysSinceLaunch%mod2.length]}</h2>
         <h2>{mod3[daysSinceLaunch%mod3.length]}</h2>
-        <h4>Daily reset: 09:00AM Pacific Time(UTC -8)</h4>
         </p>
         <br/>
-        <h5>Up next are:</h5>
-        <h5>{mod1[(Math.floor(((daysSinceLaunch+1)+elementalOffsetHack)/7))%mod1.length]} + {mod2[(daysSinceLaunch+1)%mod2.length]} + {mod3[(daysSinceLaunch+1)%mod3.length]}</h5>
+        <h4>Up next are:</h4>
+        <h4>{mod1[(Math.floor(((daysSinceLaunch+1)+elementalOffsetHack)/7))%mod1.length]} + {mod2[(daysSinceLaunch+1)%mod2.length]} + {mod3[(daysSinceLaunch+1)%mod3.length]}</h4>
+        <h5>Daily reset: 09:00AM Pacific Time(UTC -8)</h5>
+
       </div>
     );
   }
