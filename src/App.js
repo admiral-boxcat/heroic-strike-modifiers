285cd46dd9c6ce20b95f0709c947fe8b3901e881
admiral-boxcat <username@imac.lan>
2018-07-14 10:21:24 -0400

    basic daily modifier status listing

--- src/App.js
+++ src/App.js
@@ -2,18 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const mod1 = ["Void","Arc","Solar"];
+const mod2 = ["Grenadier","Brawler","Heavyweight"];
+const mod3 = ["Iron","Blackout","Grounded","Glass"]
+
+let date = new Date();
+//making sure it resets at 9am Pacific Time
+let bungieTime = date.getTime() - 57600000;
+//pushing the elemental singes over one day
+const elementalOffsetHack = 1;
+//September 6th, 2017
+const destinyLaunchDay = 1504656000000;
+let daysSinceLaunch = Math.floor((bungieTime-destinyLaunchDay)/86400000);
+let weeksSinceLaunch = Math.floor((daysSinceLaunch+elementalOffsetHack)/7);
+
+let resetTime = new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(),12);
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Daily Heroic Strike Modifiers</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <h2>{mod1[weeksSinceLaunch%mod1.length]}</h2>
+        <h2>{mod2[daysSinceLaunch%mod2.length]}</h2>
+        <h2>{mod3[daysSinceLaunch%mod3.length]}</h2>
+        <h4>Daily reset: 12:00PM Pacific Time(UTC -8)</h4>
         </p>
       </div>
+
     );
   }
 }
